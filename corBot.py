69f2854bc878688b05cefbbc74d36e46b009537d
Tim Kaiser <60766886+Tim-Kaiser@users.noreply.github.com>
2021-08-25 17:58:12 +0200

    updated with new data
    changed database to an up to date one and added extra csv parsing for more information in the bot messages

--- corBot.py
+++ corBot.py
@@ -3,16 +3,11 @@ from discord.ext import commands
 import csv
 import pandas as pd
 from datetime import date
+import math
 
+fullCSV = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv'
 
-newCases = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/ecdc/new_cases.csv'
-newDeaths = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/ecdc/new_deaths.csv'
-totalCases = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/ecdc/total_cases.csv'
-
-
-listOfCountries = 'Austria, Belgium, Brazil, Canada, China, France, Germany, India, Italy, Japan, Russia, Spain, Sweden, Thailand, United States, United Kingdom'
-
-listOfCommands = '!daily: prints new cases, new deaths, and total cases worldwide \n!countries: prints all available countries \n!country [COUNTRY NAME]: prints new cases, new deaths, and total cases in the specified country'
+listOfCommands = '!daily: prints new cases, new deaths, and total cases worldwide \n!country [COUNTRY NAME]: prints new cases, new deaths, and total cases in the specified country, reproduction rate, vaccination status, and population'
 client = commands.Bot(command_prefix = '!')
 
 @client.event
@@ -21,43 +16,60 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    #if message.author == client.user:
-        #return
-    #id = client.get_guild(225690515323092993)
-    newCasesParsed = parse_csv(newCases, 'World')
-    newDeathsParsed = parse_csv(newDeaths, 'World')
-    totalCasesParsed = parse_csv(totalCases, 'World')
-    res = '     {}: \nNew Cases: {} \nNew Deaths: {} \nTotal Cases: {}'.format( date.today(), newCasesParsed, newDeathsParsed, totalCasesParsed)
-
+    if message.author == client.user:
+        return
     if message.content.startswith("!daily"):
-        await message.channel.send(res)
+        await message.channel.send(daily_stats())
     elif message.content.startswith("!help"):
         await message.channel.send('List of commands:\n' + listOfCommands)
-    elif message.content.startswith("!countries"):
-        await message.channel.send(listOfCountries)
     elif message.content.startswith("!country"):
-        await message.channel.send(countryStat(message.content))
+        await message.channel.send(country_stats(message.content))
 
-def parse_csv(link, col):
-    file = pd.read_csv(link)
-    parsedData = file.tail(1)
-    parsedData = list(set(parsedData[col]))
-    parsedData = parsedData[0]
-    return parsedData
+def daily_stats():
+    df = pd.read_csv(fullCSV)
+    totalCasesWorld = 0;
+    newCasesWorld = 0;
+    newDeathsWorld = 0;
 
-def countryStat(country):
+    world = df["total_cases"]
+    for x in world:
+        if not math.isnan(x):
+            totalCasesWorld += x
+    world = df["new_cases"]
+    for x in world:
+        if not math.isnan(x):
+            newCasesWorld += x
+
+    world = df["new_deaths"]
+    for x in world:
+        if not math.isnan(x):
+            newDeathsWorld += x
+    res = '{}: \nNew Cases: {} \nNew Deaths: {} \nTotal Cases: {}'.format( date.today(), newCasesWorld, newDeathsWorld, totalCasesWorld)
+    return res
+
+
+def country_stats(country):
     country = country.split("!country ")
     country = country[1].capitalize()
-    if country == 'United states' or country == 'Usa':
-        country = 'United States'
 
-    if country == 'United kingdom' or country == 'Uk':
-        country = 'United Kingdom'
-    newCasesCountry = parse_csv(newCases, country)
-    newDeathsCountry = parse_csv(newDeaths, country)
-    totalCasesCountry = parse_csv(totalCases, country)
-    countryStats = '       {}: \nNew Cases: {} \nNewDeaths {} \nTotal Cases: {}'.format(country, newCasesCountry, newDeathsCountry, totalCasesCountry)
-    return countryStats
+    df = pd.read_csv(fullCSV)
 
-#insert bot ID in brackets
+    row = df.index[df['location']==country].tolist()
+    if len(row) == 0:
+        res = 'Stats for the country {} could not be found'.format(country)
+        return res
+    index = row[0]
+    country_total = df.iat[index, 4]
+    country_new = df.iat[index, 5]
+    country_deaths = df.iat[index, 7]
+    country_r = df.iat[index, 16]
+    country_fullyvaccinated = df.iat[index, 36]
+    country_pop = df.iat[index, 46]
+
+    res = '{}: \nTotal Cases: {} \nNew Cases Today: {} \nTotal Deaths: {} \nReproduction Rate: {} \nFully Vaccinated: {} \nPopulation: {}'.format( date.today(), country_total, country_new, country_deaths,country_r,country_fullyvaccinated,country_pop)
+    return res
+
+
+#insert bot ID here
 client.run()
+
